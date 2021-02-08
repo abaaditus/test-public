d0fc87f18672fde83630d6143e81cd9b4f591a3f
kevin-villarojo <kevin.villarojo@hcl.com>
2021-02-09 05:02:43 +0800

    Added a groovy file

---
+++ pipeline.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+    tools { nodejs "nodejs"}
+    stages {
+        stage('Pull files'){
+            steps {
+                echo 'This is a groovy file'
+                echo 'Pulling Jotto..'
+                git branch: 'main', credentialsId: '84bd03a8-d6a6-43fc-9a01-6ab100ac3b31', url: 'https://github.com/abaaditus/jotto.git'
+            }
+        }
+        stage('Build'){
+            steps {
+                echo 'Installing nodeJs modules...'
+                sh 'echo $PATH'
+                sh 'npm i'
+            }
+        }
+        stage('TEST'){
+            steps {
+                echo 'Running unit test...'
+                sh 'npm run test'
+            }
+        }
+    }
+}
